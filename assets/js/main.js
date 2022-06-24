8ac243350cdc31d6d6936baeeb95525707de1243
Ahmed attia <toosoo234@gmail.com>
2022-06-24 13:09:08 +0200

    1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,69 @@
+let tl = gsap.timeline();
+
+tl.to(".scroll", {
+  y: 50,
+  yoyo: true,
+  ease: "power4.inOut",
+  duration: 1.5,
+  repeat: -1,
+});
+
+let menu = document.querySelector(".menu");
+overlay = document.querySelector(".overlay");
+navContainer = document.querySelector(" ul");
+navLinks = document.querySelectorAll(" ul li a");
 
 
-let tl = gsap.timeline()
-tl.fromTo(".copyright",{
-  y:"100vh",
-  height:"30vh",
-},{
-  y:"-100vh",
-  stagger:.2,
+let timeline = gsap.timeline();
+document.querySelector(".menu").addEventListener("click", () => {
+  menuAnimation()
+  if(menu.classList.contains("inactive")){
+    menu.classList.add("active")
+    menu.classList.remove("inactive")
+    timeline.restart()
+    timeline.timeScale(1)
+
+  }
+  else if(menu.classList.contains("active")){
+    timeline.timeScale(3)
+    timeline.reverse()
+    menu.classList.add("inactive")
+    menu.classList.remove("active")
+  }
+});
+
+
+function menuAnimation(){
+  
+  timeline.to(navContainer, {
+    keyframes:{
+      "20%": {y:"50%",x:"50%",backgroundColor:"#000000db"},
+      "100%":  {left:"-50%",top:"-50%",borderRadius:0, width:"100vw",height:"100vh",trasnformOrigin:"center center"},
+    },
   duration:2,
-  repeatDelay: 0,
-  ease:"power2.inOut",
-  immediateRender:true
-})
-tl.fromTo(".line2 , .line4 , .line6",{
-  y:"100vh",
-  height:"30vh",
-},{
-  y:"-100vh",
-  stagger:.2,
-  duration:2,
-   repeatDelay: 0,
-  ease:"power2.inOut",
-  immediateRender:true
-})
-
-  .fromTo(".line1 , .line3 , .line5",{
-  y:"100vh",
-},{
-  y:"-100vh",
-  stagger:.3,
-  duration:2,
-  repeatDelay: 0,
-  ease:"linear",
-  ease:"power2.inOut",
-  immediateRender:true
-},"<")
-
-
-  .to(".line",{
-  y:"0",
-  stagger:.1,
-  duration:1.5,
-  height:"70vh",
-  ease:"power4.inOut",
-})
-
-  .fromTo(".darkline",{
-    height:0,
-    display:"none",
-}, {
-  display:"block",
-  height:"30vh",
-  duration:2,
-  ease:"power4.inOut",
-})
-
-  .to(".eye",{
-    display:"block",
-})
-  .to(".title",{
-    y:-20,
-    opacity:1,
-    duration:1,
-  ease:"power4.inOut",
-  delay:1
-})
-  .from("nav",{
-   y:-100 
+  ease:"power4.inOut"
   })
-  .from("h3",{
-   y:100 
-  })
+
+  timeline.to(".left-x", {
+    transformOrigin:"center center",
+    rotate:120,
+    y:-10
+  },"<1");
+  timeline.to(".right-x", {
+    transformOrigin:"center center",
+    rotate:-120,
+  },"<");
+  timeline.to(".hidden-x", {
+    opacity:0
+  },"<");
+  timeline.to(navLinks, {
+    keyframes:{
+      "0%": {display:"block",opacity:0, stagger:0},
+      "50%": {opacity:1,stagger:.5},
+      "100%": {y:-50,delay:1, stagger:.5},
+    },
+    ease:"power4.inOut",
+    duration:4,
+   
+   },"<");
+}
